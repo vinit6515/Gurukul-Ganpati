cef926a8617f1e77a4f2eaf46995cccede68624f
vinit6515 <143284532+vinit6515@users.noreply.github.com>
2024-09-01 12:16:00 +0530

    image handling and bio change

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,16 @@ const Home = () => {
       <img src={ganapatiImage}  alt="Ganapati Festival" className="banner-image" />
       <h2>Welcome to the Ganapati Festival 2024</h2>
       <p>
-        Join us in celebrating the auspicious Ganapati Festival in our society premises from 7th to 11th September 2024.
+      Gurukul Ganapati Festival <br></br>
+Celebrating Ganesh Utsav with Unity and Devotion!<br></br>
+
+Welcome to the official page of the Gurukul Ganapati Festival,<br></br> a vibrant celebration brought to you by the Gurukul Youth Committee! Each year, we come together as a community to honor Lord Ganesha with enthusiasm, devotion, and a host of exciting events.<br></br>
+Join us for a 5-day grand celebration filled with music, dance, cultural performances, workshops, competitions, and more. It’s a time to connect, rejoice, and make beautiful memories with our Gurukul family.<br></br>
+
+Stay tuned for event schedules, updates, and glimpses of the festivities!<br></br>
+
+Ganpati Bappa Morya!<br></br>
+Organized by the Gurukul Youth Committee
       </p>
       <p>
         Experience the joy, cultural events, competitions, and delicious dinners with your loved ones. Stay tuned for the detailed schedule of events!
